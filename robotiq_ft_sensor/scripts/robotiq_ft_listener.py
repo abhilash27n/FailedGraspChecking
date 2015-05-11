8bcd81cb9f8f2dd7cecf7808fd64cc9278bb0864
Abhilash N <abhilash.27n@gmail.com>
2015-05-11 11:40:44 -0400

    Update robotiq_ft_listener.py
    added check grasp function to use data before and after grasp and tell what has happened.

--- robotiq_ft_sensor/scripts/robotiq_ft_listener.py
+++ robotiq_ft_sensor/scripts/robotiq_ft_listener.py
@@ -1,62 +1,38 @@
 #!/usr/bin/env python
 import rospy
 from robotiq_force_torque_sensor.msg import ft_sensor
-
-# GraspStatus
-# 0 = ignore
-# 1 = before grasp
-# 2 = after grasp
-graspStatus = 0;
-
-def callback(data):
-
-    global graspStatus;
-    #rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.Rf)
-    #rospy.loginfo("Resultant force: %s", data.Rf)
-    # if(graspStatus == 0):
-    #     rospy.loginfo("IGNORING MSG");
-    # elif(graspStatus == 1):
-    #     rospy.loginfo("Resultant force before grasp: %s", data.Rf)
-    # elif(graspStatus == 2):
-    #     rospy.loginfo("Resultant force after grasp: %s", data.Rf)
     
 def listener():
 
     rfBeforeGrasp = 0;
     rfAfterGrasp = 0;
-    rfDiff = 0;
-    global graspStatus;
+    
     # In ROS, nodes are uniquely named. If two nodes with the same
     # node are launched, the previous one is kicked off. The
     # anonymous=True flag means that rospy will choose a unique
     # name for our 'listener' node so that multiple listeners can
     # run simultaneously.
+    rospy.loginfo("##STARTING##");
     rospy.init_node('robotiq_ft_listener_py', anonymous=True)
 
     #rospy.Subscriber("robotiq_force_torque_sensor", ft_sensor, callback, queue_size = 1)
 
-    rospy.loginfo("Ignore message");
-    graspStatus = 0;
-    rospy.sleep(2);
-    msg = rospy.wait_for_message("robotiq_force_torque_sensor", ft_sensor);
-    rospy.loginfo("Ignored force: %f",msg.Rf);
-
+    
     rospy.loginfo("Before grasp");
-    graspStatus = 1;
     rospy.sleep(2);
-    msg = rospy.wait_for_message("robotiq_force_torque_sensor", ft_sensor);
-    rfBeforeGrasp = msg.Rf;
+    msgBeforeGrasp = rospy.wait_for_message("robotiq_force_torque_sensor", ft_sensor);
+    rfBeforeGrasp = msgBeforeGrasp.Rf;
     rospy.loginfo("Before grasp: %f",msg.Rf);
 
     rospy.loginfo("After grasp");
     graspStatus = 2;
     rospy.sleep(2);
-    msg = rospy.wait_for_message("robotiq_force_torque_sensor", ft_sensor);
-    rfAfterGrasp = msg.Rf;
+    msgAfterGrasp = rospy.wait_for_message("robotiq_force_torque_sensor", ft_sensor);
+    rfAfterGrasp = msgAfterGrasp.Rf;
     rospy.loginfo("After grasp: %f",msg.Rf);
 
-    rfDiff = rfAfterGrasp - rfBeforeGrasp;
-    rospy.loginfo("Rf Diff: %f", rfDiff)
+    checkGrasp(msgBeforeGrasp, msgAfterGrasp);
+    rospy.loginfo("##DONE##");
 
     # spin() simply keeps python from exiting until this node is stopped
 
@@ -65,4 +41,32 @@ if __name__ == '__main__':
     listener()
 
 def checkGrasp(msgBeforeGrasp, msgAfterGrasp, expectedWeight = 0):
-    #TODO
+
+    #For now will use only Resultant for Rf from received msg, though the msg has all the sensor information
+    threhsold = 10;
+    error = 2;
+
+    #diffRf before and after grasp to use for computation
+    diffRf = msgAfterGrasp.Rf - msgBeforeGrasp.Rf;
+
+    #if no weight is sent will use threshold to make decision, else will use value within error.
+    if(expectedWeight == 0):
+
+        #if rfDiff > threshold too much weight, if < 0, anamoly
+        if(rfDiff > threshold):
+            rospy.loginfo("Looks like I'm carrying too much weight");
+        elif(rfDiff < 0):
+            rospy.loginfo("Anamoly. After weight < Before weight");
+        else:
+            rospy.loginfo("Grasp looks good");
+
+    else:
+        #if rfDiff within error of weight, good, > weight, too heavy, < weight, too light
+
+        if((rfDiff > expectedWeight - error) and (rfDiff < expectedWeight + error)):
+            rospy.loginfo("Grasp looks good"):
+        elif(rfDiff <= expectedWeight - error):
+            rospy.loginfo("Less than expected weight. Haven't picked up object");
+        elif(rfDiff >= expectedWeight + error):
+            rospy.loginfo("More than expected weight. Picked the wrong object or hit something");
+            
